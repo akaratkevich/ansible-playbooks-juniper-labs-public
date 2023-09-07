4f0c73f1f7c1152d315e45ef4defd6c2540d8a75
anton <karatkevich.anton@gmail.com>
2023-09-07 15:23:05 +0100

    updated folders

---
+++ playbooks/captures/filter_age_from_routes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Specify the directory containing the files
+CAPTURES_DIR="$SCRIPT_DIR"
+
+# Use find to get a list of XML files
+find "$CAPTURES_DIR" -type f -name '*.xml' -exec sh -c 'grep -v "<age seconds=.*</age>" "$0" > "${0%.xml}_tmp.xml"' {} \;
+
